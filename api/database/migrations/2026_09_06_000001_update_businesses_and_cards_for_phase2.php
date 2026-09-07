bb3e1f556cabc0d7729895f419da652146f09a9b
ivar79 <iska1398@gmail.com>
2026-09-07 12:16:33 -0700

    phase6n

--- api/database/migrations/2026_09_06_000001_update_businesses_and_cards_for_phase2.php
+++ api/database/migrations/2026_09_06_000001_update_businesses_and_cards_for_phase2.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    public bool $withinTransaction = false;
+    public $withinTransaction = false;
 
     public function up(): void
     {
